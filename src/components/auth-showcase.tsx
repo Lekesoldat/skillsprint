4a0c557336ab85f0f10ade4afffe98d6a50055e8
kharann <anhkhav@gmail.com>
2023-01-09 15:50:48 +0100

    add error handling of retrieving users

--- src/components/auth-showcase.tsx
+++ src/components/auth-showcase.tsx
@@ -1,26 +1,26 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { signOut, signIn, useSession, getSession } from "next-auth/react";
-import { trpc } from "../../../utils/trpc";
+import { signOut, signIn, getSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
 
 export default function AuthShowcase() {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-  const res = useSession();
   const { data: sessionData } = useQuery({
     queryKey: ["session"],
     queryFn: () => getSession(),
   });
+
+  const { data: message } = trpc.auth.getSecretMessage.useQuery(undefined, {
+    enabled: !!sessionData,
+  });
+
   return (
     <div className="flex flex-col items-center justify-center gap-2">
-      {JSON.stringify(res)}
       {sessionData && (
         <p className="text-2xl text-blue-500">
           Logged in as {sessionData?.user?.name}
         </p>
       )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
+      {message && <p className="text-2xl text-blue-500">{message}</p>}
       <button
         className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
         onClick={sessionData ? () => signOut() : () => signIn()}
