0a095258f5f9a44c1bc70ab5227afa0d1e013fcc
kharann <hey@akvo.no>
2023-01-16 09:44:08 +0100

    add cva for components

---
+++ src/components/button.tsx
@@ -0,0 +1,14 @@
+import { cva } from "class-variance-authority";
+
+export const Button = () => {
+  return <button className={button({ color: "blue" })}>Hello World</button>;
+};
+
+const button = cva(["font-semibold", "border", "rounded"], {
+  variants: {
+    color: {
+      blue: "bg-blue-500",
+      red: "bg-red-500",
+    },
+  },
+});
