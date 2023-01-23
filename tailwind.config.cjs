6a6cf5e08dd22971d0504702b3227f20f9f47adf
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-01-23 13:31:18 +0100

    Button component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "2-hard": "0px 2px",
+        "4-hard": "0px 4px",
+        "2-skew": "2px 2px",
+        "4-skew": "4px 4px",
+      },
+    },
+    colors: {
+      brand: {
+        green: "#22CA94",
+        blue: "#4B83F9",
+        red: "#FE5150",
+        pink: "#F8BED4",
+        yellow: "#FDDD6D",
+        purple: "#C8BEFD",
+        white: "#FFFFFF",
+        black: "#000000",
+        gray: "#87898C",
+      },
+    },
   },
   plugins: [],
 };
