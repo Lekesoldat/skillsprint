47dcfd3ab09e63f74d79c180cbea9269b569d1bb
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-01-23 15:53:04 +0100

    Add logo

--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -1,11 +1,12 @@
 import { BookOpen, LineChart, Medal, Trophy } from "lucide-react";
+import Image from "next/image";
 import { Points } from "../Points";
 export const TopNav = () => {
   return (
     <nav className="w-full py-4">
       <div className="container mx-auto flex items-center justify-between">
         {/* Brand Icon */}
-        <div>Icon</div>
+        <Image src={"/Logo.svg"} alt="logo" width={40} height={40} />
 
         {/* Links */}
         <ul className="flex gap-x-8 uppercase md:flex-row">
