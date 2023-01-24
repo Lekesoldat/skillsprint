59c8b62089d55ed8b65ff33bee57c9af4e0e488b
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-01-24 14:33:54 +0100

    Linting

--- src/components/AuthShowcase.tsx
+++ src/components/AuthShowcase.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { signOut, signIn, getSession } from "next-auth/react";
+import { getSession, signIn, signOut } from "next-auth/react";
 
 export default function AuthShowcase() {
   const { data: sessionData } = useQuery({
@@ -11,13 +11,13 @@ export default function AuthShowcase() {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       {sessionData && (
-        <p className="text-2xl text-blue-500">
+        <p className="text-blue-500 text-2xl">
           Logged in as {sessionData?.user?.name}
         </p>
       )}
       {JSON.stringify(sessionData)}
       <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
+        className="border-black bg-violet-50 hover:bg-violet-100 rounded-md border px-4 py-2 text-xl shadow-lg"
         onClick={sessionData ? () => signOut() : () => signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
