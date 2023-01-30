672048606b368b96cdd0e5e625dd41d4ff56eafb
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-01-30 11:16:42 +0100

    Move UI elements

--- src/pages/insight.tsx
+++ src/pages/insight.tsx
@@ -1,5 +1,5 @@
-import { Badge } from "../components/Badge";
-import { Button } from "../components/Button";
+import { Badge } from "../components/ui/Badge";
+import { Button } from "../components/ui/Button";
 
 export default function Page() {
   return (
