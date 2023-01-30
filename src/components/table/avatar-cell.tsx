418bcdced0b3bdef060b5b318964173baacf63c6
kharann <hey@akvo.no>
2023-01-30 10:37:12 +0100

    remove capitalize util

--- src/components/table/avatar-cell.tsx
+++ src/components/table/avatar-cell.tsx
@@ -1,7 +1,6 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import type { CellContext } from "@tanstack/react-table";
 import type { FC } from "react";
-import { capitalize } from "../../utils/classnames";
 import type { Person } from "./table";
 
 interface AvatarCellProps {
@@ -16,8 +15,8 @@ export const AvatarCell: FC<AvatarCellProps> = ({ info }) => {
           src={info.getValue()}
           alt={info.row.original.name.slice(0, 2)}
         />
-        <AvatarFallback>
-          {capitalize(info.row.original.name.slice(0, 2))}
+        <AvatarFallback className="capitalize">
+          {info.row.original.name.slice(0, 2)}
         </AvatarFallback>
       </Avatar>
     </div>
