9ed5f1f8fabb2a78490374876c212303b306a8f7
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-06 11:55:39 +0100

    Add timings on sort function

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,5 @@
+import { differenceInSeconds } from "date-fns";
+
 export const roundToTenthMinute = (date: Date) => {
   const coeff = 1000 * 60 * 10;
 
@@ -5,6 +7,8 @@ export const roundToTenthMinute = (date: Date) => {
 };
 
 export const sortAndAggretatePoints = (map: Map<string, number>) => {
+  const start = new Date();
+  console.log("Starting sort");
   let prev = 0;
   const liste = [...map.entries()]
     .sort((a, b) => a[0].localeCompare(b[0]))
@@ -14,5 +18,6 @@ export const sortAndAggretatePoints = (map: Map<string, number>) => {
       return [time, prev] as const;
     });
 
+  console.log(`Completed sort in ${differenceInSeconds(start, new Date())}s`);
   return liste;
 };
