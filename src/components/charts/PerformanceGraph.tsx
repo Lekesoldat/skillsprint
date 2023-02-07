b332ca3728ec8b5259ceb2d2645d88aab57f825c
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-07 12:32:19 +0100

    Begin work on performance graph

---
+++ src/components/charts/PerformanceGraph.tsx
@@ -0,0 +1,30 @@
+import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { api } from "../../utils/api";
+
+export const PerformanceGraph = () => {
+  const { data, error } = api.taskAttempt.getSuccessGrouped.useQuery();
+
+  if (error) return <>Error..</>;
+  if (!data) return <>Loading..</>;
+
+  return (
+    <ResponsiveContainer width="100%" aspect={4.0 / 3.0}>
+      <LineChart
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <XAxis dataKey="timestamp" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="groupPoints"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="userPoints" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
