4b60bf0a843e5e66658d304a078bf81f92868866
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-07 13:23:48 +0100

    Size chart

--- src/components/charts/PerformanceGraph.tsx
+++ src/components/charts/PerformanceGraph.tsx
@@ -1,4 +1,12 @@
-import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { api } from "../../utils/api";
 
 export const PerformanceGraph = () => {
@@ -8,11 +16,12 @@ export const PerformanceGraph = () => {
   if (!data) return <>Loading..</>;
 
   return (
-    <ResponsiveContainer width="100%" aspect={4.0 / 3.0}>
+    <ResponsiveContainer width="50%" aspect={4.0 / 2.0}>
       <LineChart
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 5, right: 30, left: 30, bottom: 5 }}
       >
+        {/* <CartesianGrid strokeDasharray="3 3" /> */}
         <XAxis dataKey="timestamp" />
         <YAxis />
         <Tooltip />
