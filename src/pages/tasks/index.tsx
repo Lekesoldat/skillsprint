96aa83579d4e8030621fcb518e8a170d479e64b4
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-09 17:54:53 +0100

    Spice up task page

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -2,19 +2,28 @@ import Link from "next/link";
 import { api } from "../../utils/api";
 
 export default function Task() {
-  const { data, isError } = api.task.getAll.useQuery();
+  const { data, isError } = api.category.getCategoriesAndTasks.useQuery();
 
   if (isError) return <>Error bruvv...</>;
 
   if (!data) return <>Loading...</>;
 
   return (
-    <>
-      {data.map((task) => (
-        <Link key={task.id} href={`/tasks/${task.id}`}>
-          <p>{task.title}</p>
-        </Link>
+    <div className="grid h-full grid-cols-4">
+      {data.map((d) => (
+        <div key={d.id}>
+          <div>{d.name}</div>
+          <div>
+            {d.tasks.map((t) => (
+              <Link key={t.id} href={`/tasks/${t.id}`}>
+                <p>
+                  {t.title} - {t.points}p
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
