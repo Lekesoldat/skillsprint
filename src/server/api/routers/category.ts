c40bc338b69234beba051f5c53a34f65b5278051
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-09 17:55:33 +0100

    Create endpoint for tasks by category

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,4 +1,4 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -9,6 +9,41 @@ export const categoryRouter = createTRPCRouter({
     } catch (error) {
       throw new TRPCError({
         code: "BAD_REQUEST",
+        cause: error,
+      });
+    }
+  }),
+
+  getCategoriesAndTasks: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const res = await ctx.prisma.category.findMany({
+        include: {
+          Task: {
+            select: {
+              id: true,
+              title: true,
+              points: true,
+            },
+
+            orderBy: {
+              title: "asc",
+            },
+          },
+        },
+      });
+
+      return res.map((cat) => ({
+        id: cat.id,
+        name: cat.name,
+        tasks: cat.Task.map((t) => ({
+          id: t.id,
+          title: t.title,
+          points: t.points,
+        })),
+      }));
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
         cause: error,
       });
     }
