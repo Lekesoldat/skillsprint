4b1fa8fbf4a1c71c4de7b46261b0ab9c025eb693
kharann <anhkhav@gmail.com>
2023-02-16 14:36:23 +0100

    add migrations

---
+++ prisma/migrations/20230216133554_nuke/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Achievement` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Achievement";
+
+-- DropEnum
+DROP TYPE "AchievementColor";
