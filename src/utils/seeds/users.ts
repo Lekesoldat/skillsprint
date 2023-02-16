9b5f986d9f1f0aa53ae3dfe577c64ffeaa0cc7c8
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-16 16:09:09 +0100

    Add more users

--- src/utils/seeds/users.ts
+++ src/utils/seeds/users.ts
@@ -13,8 +13,48 @@ export async function createUsers() {
     },
     {
       id: "cle2wz3id0002fxpb0digx7tv",
-      name: "holtet",
-      password: await argon2.hash("hunter2"),
+      name: "magnus",
+      password: await argon2.hash("holtet"),
+    },
+    {
+      id: "cle78e7g2000008lgen46erji",
+      name: "mathias",
+      password: await argon2.hash("fossum"),
+    },
+    {
+      id: "cle78frxv000108lg79eu7d5b",
+      name: "johanne",
+      password: await argon2.hash("tronstad"),
+    },
+    {
+      id: "cle78g83p000208lgdy2m3o0w",
+      name: "øyvind",
+      password: await argon2.hash("schjerven"),
+    },
+    {
+      id: "cle78gkpq000308lghus0fu6s",
+      name: "kristoffer",
+      password: await argon2.hash("nyvoll"),
+    },
+    {
+      id: "cle78hhqv000408lgg89n28b9",
+      name: "carl",
+      password: await argon2.hash("smestad"),
+    },
+    {
+      id: "cle78hnzv000508lg0i9yc4rz",
+      name: "andreas",
+      password: await argon2.hash("amundsen"),
+    },
+    {
+      id: "cle78hrrb000608lg8z2x68wr",
+      name: "kristine",
+      password: await argon2.hash("larssen"),
+    },
+    {
+      id: "cle78jrxx000708lgglob19m9",
+      name: "hilde",
+      password: await argon2.hash("haug"),
     },
   ];
 
