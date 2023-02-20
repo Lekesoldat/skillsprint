069a0690d4edf24aebaa651d33341007e40d3a63
kharann <anhkhav@gmail.com>
2023-02-20 16:59:38 +0100

    start on toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import AuthShowcase from "../components/AuthShowcase";
+import { Button } from "../components/ui/Button";
+import { useToast } from "../hooks/use-toast";
 
 export default function Page() {
+  const { toast } = useToast();
   return (
     <div className="flex w-full flex-col justify-center">
       <AuthShowcase />
+      <Button
+        onClick={() => {
+          toast({
+            title: "Riktig svar!",
+            description: "+50 poeng",
+          });
+        }}
+      >
+        Show Toast
+      </Button>
     </div>
   );
 }
