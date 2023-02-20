90318c43cdf6520947bd0fb5528e6eb50ebf5529
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-20 16:33:41 +0100

    Add user to footer

--- src/components/layout/FooterContent.tsx
+++ src/components/layout/FooterContent.tsx
@@ -1,7 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSession } from "next-auth/react";
+import { format } from "date-fns";
+import { nb } from "date-fns/locale";
+
 export const FooterContent = () => {
+  const { data: sessionData } = useQuery({
+    queryKey: ["session"],
+    queryFn: () => getSession(),
+  });
+
   return (
-    <div className="flex w-full items-center justify-center text-brand-white">
-      Anh-Kha & Magnus&apos; masteroppgave @ NTNU
+    <div className="flex w-full items-center justify-between px-8 text-brand-white">
+      <div>👋, {sessionData?.user?.name}</div>
+      {/* <div className="flex flex-1 justify-center">
+        Anh-Kha & Magnus&apos; masteroppgave @ NTNU
+      </div> */}
+      <div>{format(new Date(), "eeee Mo MMMM", { locale: nb })}</div>
     </div>
   );
 };
