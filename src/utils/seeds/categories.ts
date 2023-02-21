37ed205fff946edf7b3e49b2f908cd5cae0772ee
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-21 22:34:42 +0100

    Temporarily remove certain categories

--- src/utils/seeds/categories.ts
+++ src/utils/seeds/categories.ts
@@ -7,7 +7,7 @@ export async function createCategories({
 }) {
   console.info("\n📒 Seeding categories...");
   const data: Prisma.CategoryCreateInput[] = [
-    { id: "cldacdi520000sbxe8eyqu26y", name: "Algebra" },
+    // { id: "cldacdi520000sbxe8eyqu26y", name: "Algebra" },
     { id: "cldacdi520001sbxekmyct9yd", name: "Lineære funksjoner" },
     { id: "cleeb63a5000008judykn4061", name: "Kvadratiske funksjoner" },
     { id: "cleebx9wi000508jt5ik9fgpx", name: "Eksponentialfunksjon" },
@@ -16,7 +16,7 @@ export async function createCategories({
       name: "Omvendt proporsjonale funksjoner",
     },
     { id: "cldacdi530002sbxeu6rgijzl", name: "Likninger" },
-    { id: "cldx7ez8a000008juf4sl1wdq", name: "Økonomi" },
+    // { id: "cldx7ez8a000008juf4sl1wdq", name: "Økonomi" },
     { id: "cle791t0y000308ml7zsw59wo", name: "Noobs" },
   ];
 
