37d2e49e2b085bfb015b7395f98726b8ff111a84
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-23 12:12:56 +0100

    Create PictureTask, TaskNavigation, TextTask

--- src/components/task-answer/TextTask.tsx
+++ src/components/task-answer/TextTask.tsx
@@ -1,3 +1,73 @@
-export const TextTask = () => {
-  return <>Im a text task</>;
+import type { TaskAttempt } from "@prisma/client";
+import type { Control } from "react-hook-form";
+import type {
+  FormValues,
+  SubmitHandler,
+  TaskPageProps,
+} from "../../pages/tasks/[id]";
+import { MathDisplay } from "../math/MathDisplay";
+import { MathInput } from "../math/MathInput";
+import { Badge } from "../ui/Badge";
+import { Button } from "../ui/Button";
+import { TaskNavigation } from "./TaskNavigation";
+
+interface Props {
+  task: Omit<TaskPageProps["task"], "image">;
+  attempt: TaskAttempt | undefined;
+  form: {
+    submitHandler: SubmitHandler;
+    control: Control<FormValues, any>;
+  };
+  isLoading: boolean;
+  isAnswering: boolean;
+}
+export const TextTask = ({
+  task,
+  attempt,
+  form: { submitHandler, control },
+  isAnswering,
+  isLoading,
+}: Props) => {
+  return (
+    <>
+      <div className="mt-0 flex flex-col items-center justify-center gap-4 rounded-md border-2 border-brand-purple bg-gradient-radial from-[rgba(217,217,217,0.12)] to-white bg-cover px-10 py-10 shadow-4-right shadow-brand-purple lg:mt-10 lg:gap-8 lg:px-0">
+        <div className="text-4xl font-bold text-brand-green lg:text-5xl">
+          {task.title}
+        </div>
+        <Badge text={task.category.name} />
+        <div className="flex max-w-[75ch] flex-col items-center rounded-md bg-white p-4">
+          <MathDisplay description={task.description} />
+        </div>
+        <form className="grid gap-6" onSubmit={submitHandler}>
+          {attempt?.result === "SUCCESS" ? (
+            <div className="flex w-full justify-center">
+              <div className="rounded-lg border-2 bg-brand-white p-4">
+                <MathDisplay description={`math$${task.answer}&`} />
+              </div>
+            </div>
+          ) : (
+            <MathInput control={control} name="answer" />
+          )}
+
+          {/* If task is answered */}
+          {attempt?.result === "SUCCESS" ? (
+            <p className="mb-4 font-medium text-brand-green">
+              Denne oppgaven er fullført! Svaret ser du over.
+            </p>
+          ) : (
+            <Button type="submit" loading={isLoading || isAnswering}>
+              Sjekk svar
+            </Button>
+          )}
+
+          {attempt?.result === "SUCCESS" && (
+            <TaskNavigation
+              prevTaskId={task.prevTask?.id}
+              nextTaskId={task?.nextTaskId}
+            />
+          )}
+        </form>
+      </div>
+    </>
+  );
 };
--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -4,14 +4,11 @@ import type {
   GetStaticProps,
   InferGetStaticPropsType,
 } from "next";
-import Image from "next/image";
-import Link from "next/link";
+import type { BaseSyntheticEvent } from "react";
 import { useForm } from "react-hook-form";
 import superjson from "superjson";
-import { MathDisplay } from "../../components/math/MathDisplay";
-import { MathInput } from "../../components/math/MathInput";
-import { Badge } from "../../components/ui/Badge";
-import { Button } from "../../components/ui/Button";
+import { PictureTask } from "../../components/task-answer/PictureTask";
+import { TextTask } from "../../components/task-answer/TextTask";
 import { useToast } from "../../hooks/use-toast";
 import { appRouter } from "../../server/api/root";
 import { createInnerTRPCContext } from "../../server/api/trpc";
@@ -20,12 +17,17 @@ import { api } from "../../utils/api";
 
 export type TaskPageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
+export type SubmitHandler = (
+  e?: BaseSyntheticEvent<object, any, any> | undefined
+) => Promise<void>;
+
 export type FormValues = {
   answer: string;
 };
 
 export default function TaskPage({ task }: TaskPageProps) {
   const { handleSubmit, control } = useForm<FormValues>();
+
   const utils = api.useContext();
   const { toast } = useToast();
 
@@ -71,66 +73,23 @@ export default function TaskPage({ task }: TaskPageProps) {
   return (
     <div className="mb-[200px] mt-8 w-full lg:mb-0">
       <div className="bg-[url('/grid.svg')]">
-        <div className="mt-0 flex flex-col items-center justify-center gap-4 rounded-md border-2 border-brand-purple bg-gradient-radial from-[rgba(217,217,217,0.12)] to-white bg-cover px-10 py-10 shadow-4-right shadow-brand-purple lg:mt-10 lg:gap-8 lg:px-0">
-          <div className="text-4xl font-bold text-brand-green lg:text-5xl">
-            {task.title}
-          </div>
-          <Badge text={task.category.name} />
-          <div className="flex max-w-[75ch] flex-col items-center rounded-md bg-white p-4">
-            {task.image && (
-              <div className="relative mb-4 h-[350px] w-[350px] lg:h-[450px] lg:w-[450px]">
-                <Image
-                  src={task.image}
-                  alt={task.title}
-                  fill
-                  className="object-contain"
-                />
-              </div>
-            )}
-            <MathDisplay description={task.description} />
-          </div>
-          <form className="grid gap-6" onSubmit={submitHandler}>
-            {attempt?.result === "SUCCESS" ? (
-              <div className="flex w-full justify-center">
-                <div className="rounded-lg border-2 bg-brand-white p-4">
-                  <MathDisplay description={`math$${task.answer}&`} />
-                </div>
-              </div>
-            ) : (
-              <MathInput control={control} name="answer" />
-            )}
-
-            {/* If task is answered */}
-            {attempt?.result === "SUCCESS" ? (
-              <p className="mb-4 font-medium text-brand-green">
-                Denne oppgaven er fullført! Svaret ser du over.
-              </p>
-            ) : (
-              <Button type="submit" loading={isLoading || isAnswering}>
-                Sjekk svar
-              </Button>
-            )}
-
-            {attempt?.result === "SUCCESS" && (
-              <div className="flex justify-between">
-                {task.prevTask?.id ? (
-                  <Link href={`/tasks/${task.prevTask.id}`}>
-                    <Button size="xs">Gå tilbake</Button>
-                  </Link>
-                ) : (
-                  <div />
-                )}
-                {task.nextTaskId ? (
-                  <Link href={`/tasks/${task.nextTaskId}`}>
-                    <Button size="xs">Gå videre</Button>
-                  </Link>
-                ) : (
-                  <div />
-                )}
-              </div>
-            )}
-          </form>
-        </div>
+        {task.image ? (
+          <PictureTask
+            task={task}
+            attempt={attempt}
+            form={{ submitHandler, control }}
+            isAnswering={isAnswering}
+            isLoading={isLoading}
+          />
+        ) : (
+          <TextTask
+            task={task}
+            attempt={attempt}
+            form={{ submitHandler, control }}
+            isAnswering={isAnswering}
+            isLoading={isLoading}
+          />
+        )}
       </div>
     </div>
   );
