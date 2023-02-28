97d07247c27cae3ec540add9abc8f77606928108
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-02-28 14:35:39 +0100

    Remove bloat

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,7 @@
-// More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
-// export default withAuth({
-//   callbacks: {
-//     authorized({ req, token }) {
-//       return !!token;
-//     },
-//   },
-// });
-
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // runtime: "nodejs",
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    // "/((?!api|_next/static|_next/image|calculator.svg).*)",
     "/tasks",
     "/tasks/:path*",
     "/insight",
