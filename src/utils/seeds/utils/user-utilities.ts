63bb518a57c47301299e978454932db0a3ab39ec
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-03-02 15:09:49 +0100

    Dev (#98)
    * migrate and add answer type
    
    * Set placeholders and remove default
    
    * Design changes (see commit history for more details) (#94)
    
    * Prganize imports
    
    * Better letter spacing and center task titles
    
    * More space in achievements
    
    * More spacing between entries
    
    * Change graph interval back to every fifth minute
    
    * Add sign in/out to footer
    
    * Informative front page
    
    * Add home-button and a bit smaller spacing between
    
    * Finish placeholders
    
    * refetch streaks when you fail
    
    * Testing tasks (#97)
    
    * Update regEx to fit more task variants
    
    * Update seeds to focus on actual data. Begin adding real tasks
    
    ---------
    
    Co-authored-by: kharann <anhkhav@gmail.com>

---
+++ src/utils/seeds/utils/user-utilities.ts
@@ -0,0 +1,90 @@
+import type { Prisma, PrismaClient } from "@prisma/client";
+import { differenceInSeconds } from "date-fns";
+import argon2 from "argon2";
+
+const hashPasswords = async (userList: Prisma.UserCreateInput[]) => {
+  const userPromises = userList.map(async (user) => {
+    const hashedPassword = await argon2.hash(user.password);
+
+    return {
+      ...user,
+      password: hashedPassword,
+    };
+  });
+
+  return await Promise.all(userPromises);
+};
+
+const admins = [
+  {
+    id: "cle2wz3id0000fxpb8bgt5f3f",
+    name: "anhkha",
+    image:
+      "https://tliacbojiuhirqtqavdn.supabase.co/storage/v1/object/public/icons/avatars/047-turkey.svg",
+    password: "hunter2",
+  },
+  {
+    id: "cle2wz3id0002fxpb0digx7tv",
+    name: "magnus",
+    image:
+      "https://tliacbojiuhirqtqavdn.supabase.co/storage/v1/object/public/icons/avatars/048-vulture.svg",
+    password: "holtet",
+  },
+  {
+    id: "cle78jrxx000708lgglob19m9",
+    name: "hilde",
+    image:
+      "https://tliacbojiuhirqtqavdn.supabase.co/storage/v1/object/public/icons/avatars/049-bird.svg",
+    password: "haug",
+  },
+
+  {
+    id: "cle8i7e0g000108mpaawh9cw0",
+    name: "oskar",
+    image:
+      "https://tliacbojiuhirqtqavdn.supabase.co/storage/v1/object/public/icons/avatars/049-bird.svg",
+    password: "olsen",
+  },
+  {
+    id: "cleidrxes000008lf5b55f45t",
+    name: "synne",
+    image:
+      "https://tliacbojiuhirqtqavdn.supabase.co/storage/v1/object/public/icons/avatars/049-bird.svg",
+    password: "markmanrud",
+  },
+];
+
+interface CreateUsersOptions {
+  prismaClient: PrismaClient;
+  users: Prisma.UserCreateInput[];
+  withAdmins: boolean;
+}
+
+export async function createUsers({
+  prismaClient,
+  users,
+  withAdmins = true,
+}: CreateUsersOptions) {
+  const timer = new Date();
+  console.info(`\n🎓 Seeding users ${withAdmins ? "and admins " : ""}!`);
+
+  if (withAdmins) {
+    users.push(...admins);
+  }
+
+  const output = await hashPasswords(users);
+
+  const data = await prismaClient.$transaction(
+    output.map((user) =>
+      prismaClient.user.upsert({
+        where: { name: user.name },
+        create: user,
+        update: user,
+      })
+    )
+  );
+
+  console.log(`Took ${differenceInSeconds(new Date(), timer)}s`);
+
+  return data;
+}
