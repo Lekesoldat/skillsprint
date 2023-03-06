6a36e1392ab1c17daec5ed9685d578001f4684c3
kharann <hey@akvo.no>
2023-03-06 14:54:21 +0100

    only retrieve from your session

--- src/server/api/routers/taskAttempt.ts
+++ src/server/api/routers/taskAttempt.ts
@@ -161,9 +161,6 @@ export const taskAttemptRouter = createTRPCRouter({
         ctx.prisma.task.findMany(),
         ctx.prisma.taskAttempt.findMany({
           where: {
-            result: {
-              equals: "SUCCESS",
-            },
             AND: [
               {
                 result: "SUCCESS",
