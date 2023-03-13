3865608e577249bc0e8d1902df3c135406720230
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-03-13 16:08:13 +0100

    Embed survey (#112)

---
+++ src/pages/survey.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="flex w-full place-content-center">
+      <iframe
+        src="https://nettskjema.no/a/327747?embed=1"
+        title="SkillSprint"
+        width="100%"
+      ></iframe>
+    </div>
+  );
+}
