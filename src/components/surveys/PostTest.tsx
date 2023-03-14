f3f5004b07eb382b4e5487f76049ec61a624191e
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-03-14 13:14:09 +0100

    Add surveys PreTest and PostTest

---
+++ src/components/surveys/PostTest.tsx
@@ -0,0 +1,9 @@
+export const PostTest = () => {
+  return (
+    <iframe
+      src="https://nettskjema.no/a/327747?embed=1"
+      title="SkillSprint: Post-test"
+      width="100%"
+    ></iframe>
+  );
+};
---
+++ src/components/surveys/PreTest.tsx
@@ -0,0 +1,9 @@
+export const PreTest = () => {
+  return (
+    <iframe
+      src="https://nettskjema.no/a/328152?embed=1"
+      title="SkillSprint: Pre-test"
+      width="100%"
+    ></iframe>
+  );
+};
