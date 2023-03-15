f76655b6483da893acf91963717b706cc1225332
Magnus L. Holtet <magnus.holtetx+dev@gmail.com>
2023-03-15 13:01:23 +0100

    Move survey link from footer to front-page

--- src/components/layout/FooterContent.tsx
+++ src/components/layout/FooterContent.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 import { getSession, signIn, signOut } from "next-auth/react";
-import Link from "next/link";
 
 export const FooterContent = () => {
   const { data: sessionData } = useQuery({
@@ -14,9 +13,7 @@ export const FooterContent = () => {
     <div className="flex w-full items-center justify-between px-8 text-brand-white">
       {sessionData?.user?.name && <div>👋, {sessionData?.user?.name}</div>}
 
-      <Link href={"/survey"}>
-        <div>{format(new Date(), "eeee do MMMM", { locale: nb })}</div>
-      </Link>
+      <div>{format(new Date(), "eeee do MMMM", { locale: nb })}</div>
 
       <button
         className="rounded-md py-2 px-4 transition-colors hover:bg-purple-300"
